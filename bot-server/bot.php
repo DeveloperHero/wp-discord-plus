6c6a5b22817ea89c75714575248c0298068db15b
M Yakub Mizan <ym@orchestra.ltd>
2018-09-20 15:44:21 +0600

    Bot code refactoring

--- bot-server/bot.php
+++ bot-server/bot.php
@@ -3,18 +3,22 @@
 //set_time_limit(0);
 include __DIR__.'/vendor/autoload.php';
 
+define("BOT_URL", 'http://localhost/wp-admin/admin-ajax.php');
+define("BOT_TOKEN", 'NDkyMjU5ODA3MjQ4NTE1MDcy.DoT0Pg.VUy8DsyUeZ7X_fTbOmdjdjEKA44');
+
 use Discord\Discord;
 
 $discord = new Discord([
-	'token' => 'NDkxOTc0OTUwODgyNzcwOTY4.DoQYlw.2b5G6rkyAA7EDWq53RpvUb3XPp8',
+	'token' => BOT_TOKEN
 ]);
 
 $discord->on('ready', function ($discord) {
-	echo "Bot is ready!", PHP_EOL;
+	echo "Bot listening for new message", PHP_EOL;
 
 	// Listen for messages.
 	$discord->on('message', function ($message, $discord) {
 		echo "New message from: " .  $message->author->username . "\n";
+		echo "Message: " .  $message->content . "\n";
 
 		if ($message->author->user->bot == 0)
 		{
@@ -26,7 +30,6 @@ $discord->on('ready', function ($discord) {
 				'message'  	=> $message->content, 
 			);
 
-			$URL = 'http://localhost/wp-admin/admin-ajax.php';
 			$options = array(
 			    'http' => array(
 			        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
@@ -34,13 +37,26 @@ $discord->on('ready', function ($discord) {
 			        'content' => http_build_query($data)
 			    )
 			);
-			$context  = stream_context_create($options);
-			$result = file_get_contents($URL, false, $context);
-			if ($result === FALSE) {
-				//echo "No response form WP Bot";
-			}
 
-			$message->reply($result);
+			$context  = stream_context_create($options);
+
+			try {
+
+				$result = file_get_contents(BOT_URL, false, $context);
+				echo "Response Received: " . $result . "\n";
+
+				if ($result === FALSE) {
+					echo "No response received from WP Discord\n";
+				}
+
+				if ($result != 'null' || !empty($result))
+				{
+					$message->reply($result);
+				}
+
+			} catch(Exception $e) {
+				echo "WP Error: " . $e->getMessage() . "\n";
+			}
 		}
 	});
 });
